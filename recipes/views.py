5f798b37b4032ac56a9b4da979e601ed7856e52e
George Silva <georgerodrigues886@gmail.com>
2023-03-29 07:02:02 -0300

    update

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'George Silva'
     })
 # esta pasta recipes dentro de templates e usada para dar um nome especifico ao arquivo home para o django não confundir com outro.
