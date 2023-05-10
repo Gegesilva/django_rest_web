0ef87c347ba97ee1a5f5de19e68a7a92e03fcd22
George Silva <georgerodrigues886@gmail.com>
2023-05-10 06:53:23 -0300

    update

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,6 +6,6 @@ from . import views
 # http response (servidor responde)
 urlpatterns = [
     path('', views.home),  # o caminho vazio representa a raiz do site # / home
-    path('recipes/<id>/', views.recipe)
+    path('recipes/<int:id>/', views.recipe)
     # <id> isto e o id passado na url que será enviado para a view
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,23 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 
+from util.recipes.factory import make_recipe
+
+# importando arquivo fake de teste
+
 # Create your views here.
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'George Silva'
+        'recipes': [make_recipe() for _ in range(10)],
     })
-# esta pasta recipes dentro de templates e usada para dar um nome especifico ao arquivo home para o django não confundir com outro.
+
+
+""" esta pasta recipes dentro de templates e usada para dar um nome especifico ao arquivo home para o django não confundir com outro. """
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/home.html', context={
-        'name': 'George Silva'
+    return render(request, 'recipes/partials/recipe_view.html', context={
+        recipe: make_recipe()
     })
